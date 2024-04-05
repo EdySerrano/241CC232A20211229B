bd8b45917c389e8044a2e682ec6afd4f08ff5f10
EdySerrano <edy.serrano.a@uni.pe>
2024-04-05 09:11:34 -0500

    Busqueda_Binaria

--- src/main/java/uni/aed/search/Search.java
+++ src/main/java/uni/aed/search/Search.java
@@ -6,7 +6,7 @@ package uni.aed.search;
 public class Search {
     public final int NO_ENCONTRADO=-1;
     
-    public int Lineal(Integer[] X, int valor){
+    public int searchLineal(Integer[] X, int valor){
         int loc=0;
         while(loc<X.length && X[loc]!=valor){
             loc++;
@@ -17,4 +17,18 @@ public class Search {
             return loc;
     }
     
+    public int SearchBinaria(Integer[] X, int valor){
+        int bajo=0, alto=X.length -1,//low , high
+        medio =(bajo + alto)/2;
+        while(bajo<=alto && X[medio]!=valor){
+            if (X[medio]<valor)
+                bajo=medio+1;
+            else
+                alto=medio-1;
+            medio = (bajo + alto)/2;
+        }
+        if(bajo>alto)
+            medio = NO_ENCONTRADO;
+        return medio;
+    }
 }
