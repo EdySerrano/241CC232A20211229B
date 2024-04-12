9290e24aed44cb3fe9597698a63a29b978d41803
EdySerrano <edy.serrano.a@uni.pe>
2024-04-12 08:59:42 -0500

    PC1

---
+++ src/main/java/uni/aed/241CC232A20211229BPC1/P1.java
@@ -0,0 +1,31 @@
+package 241CC232A20211229BPC1;
+/**
+ * Algoritmo que determina el Maximo y Minimo de una lista de numeros enteros
+ * @author uni.cc232
+ */
+public class P1 {    
+    
+    public Integer getMaxValor(Integer[] X){
+        if(X.length==0)
+            return -1;
+        Integer max=X[0];
+        for(int i=1;i<X.length;i++){
+            if(X[i]>max)
+                max=X[i];         
+        }
+        return max;
+    }
+    public Integer getMinValor(Integer[] X){
+        if(X.length==0)
+            return -1;
+        Integer min=X[0];
+        for(int i=1;i<X.length;i++){
+            if(X[i]<min)
+                min=X[i];         
+        }
+        return min;
+    }
+}
+public class P1 {
+    
+}
